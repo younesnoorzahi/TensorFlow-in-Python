f43c5364290ef4373d2254b0043bdbea310ec705
Younes <younes.noorzai2005@gmail.com>
2025-05-17 12:31:19 +0330

    Add files via upload

---
+++ Example-Neural-Network.py
@@ -0,0 +1,18 @@
+# Build a simple classifier
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=5)
+
+# Evaluate
+test_loss, test_acc = model.evaluate(test_images, test_labels)
